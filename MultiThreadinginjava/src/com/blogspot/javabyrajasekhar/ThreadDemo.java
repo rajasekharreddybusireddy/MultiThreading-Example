b835cbf41c484d9993aa473313f55ce68e12ac33
Rajasekhar Reddy <Rajasekhar Reddy@DESKTOP-LMUN7FU>
2018-12-11 08:09:33 +0530

    testing

--- MultiThreadinginjava/src/com/blogspot/javabyrajasekhar/ThreadDemo.java
+++ MultiThreadinginjava/src/com/blogspot/javabyrajasekhar/ThreadDemo.java
@@ -3,7 +3,7 @@ package com.blogspot.javabyrajasekhar;
 public class ThreadDemo {
 
 	public static void main(String[] args) {
-
+ 
 		Thread thread = Thread.currentThread();
 		System.out.println("Thread info :"+thread);
 	}
